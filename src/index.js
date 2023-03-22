b827d6346cae4246f3486e95bf7117b7f9f45cb1
DaveyAuz <doswald27@gmail.com>
2023-03-21 23:22:11 -0400

    app DOT js ADDED not sure what else im supposed to do im really lost.

--- src/index.js
+++ src/index.js
@@ -3,11 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://us1.locationiq.com/v1/search?key=YOUR_ACCESS_TOKEN&q=SEARCH_STRING&format=json",
+  "method": "GET"
+}
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
 
 
