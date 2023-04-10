b5948491ff0f81dcd8babc042cbc4112fe178115
DaveyAuz <doswald27@gmail.com>
2023-04-09 23:08:00 -0400

    fixed movies.js and working on bootstrap function

--- src/Movies.js
+++ src/Movies.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Movies from './Movies';
 
 export default class Movie extends Component {
     render() {
@@ -7,7 +6,8 @@ export default class Movie extends Component {
         return (
             <div>
                 <h3>Movies</h3>
-                {this.props.movieData.map(movie => <Movies movie={movie}/>)}
+                
+                {this.props.movieData.map(movie => <Movie movieData={movie}/>)}
             </div>
         );
     }
